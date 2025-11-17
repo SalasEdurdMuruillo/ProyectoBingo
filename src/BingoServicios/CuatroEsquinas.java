16622f00fe028084062b983ca81b363f7cf9cfe3
Felipe0220 <lusalaspe@est.utn.ac.cr>
2025-11-16 23:00:51 -0600

    Paquete BingoServicios
    Se creo la clase CuatroEsquinas con todos sus metodos funcionando y utilizando el patron de diseño Strategy

---
+++ src/BingoServicios/CuatroEsquinas.java
@@ -0,0 +1,12 @@
+package BingoServicios;
+
+import BingoInterfaces.Ganador;
+import BingoCarton.Carton;
+
+public class CuatroEsquinas implements Ganador {
+    @Override
+    public boolean esGanador(Carton c){
+        boolean[][] m = c.getMarcados();
+        return m[0][0] && m[0][4] && m[4][0] && m[4][4];
+    }
+}
